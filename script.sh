a87266db6a1fefc2ac363e619832c7652593251b
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-02 15:41:37 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -124,7 +124,7 @@ WEB_LINE="$(tmate -S "${TMATE_SOCK}" display -p '#{tmate_web}')"
 TIMEOUT_MESSAGE="If you don't connect to this session, it will be *SKIPPED* in ${timeout} seconds at ${kill_date}. To skip this step now, simply connect the ssh and exit."
 echo -e "$TIMEOUT_MESSAGE"
 
-if [[ -n "$TELEGRAM_TOKEN" ]]; then
+if [[ -n "${TELEGRAM_BOT_TOKEN}" && -n "${TELEGRAM_CHAT_ID}" ]]; then
   MSG="SSH: ${SSH_LINE}\nWEB: ${WEB_LINE}"
   echo -n "Sending information to Telegram Bot......"
   curl -k --data chat_id="${TELEGRAM_CHAT_ID}" --data "text=SSH: ${SSH_LINE}  WEB: ${WEB_LINE}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage"
